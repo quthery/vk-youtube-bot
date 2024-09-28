4b012d2fbf7f949b9664aff58d20e51b63698715
quthery <chiefkeef7317@gmail.com>
2024-09-28 15:42:46 +0300

    asd

--- run.py
+++ run.py
@@ -3,10 +3,10 @@ from vkbottle import Bot
 from config import settings, labeler, state_dispenser
 from app.database import create_tables
 import asyncio
-async def main():
-    await create_tables()
-
-asyncio.run(main())
+# async def main():
+#     await create_tables()
+#
+# asyncio.run(main())
 labeler.load(menu_labeler)
 labeler.load(start_labeler)
 labeler.load(download_labeler)
